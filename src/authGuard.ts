2b7ed038868afd4df3099cc721f7eee4e13fecf9
OussemaGharbi <oussema.gharbi@tek-up.de>
2022-04-19 17:36:08 +0100

    add form

--- src/authGuard.ts
+++ src/authGuard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { AuthentificationService } from "./services/authentification.service";
+import { AuthService } from "./app/services/auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate{
-    constructor(private authService: AuthentificationService,private router: Router){}
+    constructor(private authService: AuthService,private router: Router){}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         const isAuth = this.authService.isAuthentificated;
         if(isAuth==false){
